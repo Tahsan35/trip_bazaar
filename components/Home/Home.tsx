e2282e373a201c5a0b49e557ff956165db6346db
Tahan Shakil <tahsanknmas14@gmail.com>
2025-04-23 23:10:37 +0600

    completed the hero section

--- components/Home/Home.tsx
+++ components/Home/Home.tsx
@@ -1,7 +1,11 @@
-import React from "react";
+import Hero from "./Hero/Hero";
 
 const Home = () => {
-  return <div>Home page</div>;
+  return (
+    <div className="overflow-hidden">
+      <Hero />
+    </div>
+  );
 };
 
 export default Home;
