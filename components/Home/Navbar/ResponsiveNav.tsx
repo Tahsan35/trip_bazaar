117f3497d502b7fb9cc2d0972dbc71afae3f085c
Tahan Shakil <tahsanknmas14@gmail.com>
2025-04-23 17:52:26 +0600

    completed navbar

--- components/Home/Navbar/ResponsiveNav.tsx
+++ components/Home/Navbar/ResponsiveNav.tsx
@@ -1,12 +1,17 @@
-import React from "react";
+"use client";
 import Nav from "./Nav";
 import MobileNav from "./MobileNav";
+import { useState } from "react";
 
 const ResponsiveNav = () => {
+  const [showNav, setShowNav] = useState(false);
+  const handNavShow = () => setShowNav(true);
+  const handleCloseNav = () => setShowNav(false);
+
   return (
     <div>
-      <Nav />
-      <MobileNav />
+      <Nav openNav={handNavShow} />
+      <MobileNav showNav={showNav} closeNav={handleCloseNav} />
     </div>
   );
 };
