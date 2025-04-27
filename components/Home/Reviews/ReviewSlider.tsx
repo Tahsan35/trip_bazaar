5a26bc04cc49af2c9cb8a6735a8ee101b67e22bc
Tahan Shakil <tahsanknmas14@gmail.com>
2025-04-27 17:22:54 +0600

    working on review slider

---
+++ components/Home/Reviews/ReviewSlider.tsx
@@ -0,0 +1,12 @@
+// import Swiper JS
+import Swiper from 'swiper';
+// import Swiper styles
+import 'swiper/css';
+
+const swiper = new Swiper(...);
+
+const ReviewSlider = () => {
+  return <div>ReviewSlider</div>;
+};
+
+export default ReviewSlider;
--- components/Home/Reviews/Reviews.tsx
+++ components/Home/Reviews/Reviews.tsx
@@ -1,7 +1,44 @@
-import React from "react";
+import ReviewSlider from "./ReviewSlider";
+import { FaStar } from "react-icons/fa";
 
 const Reviews = () => {
-  return <div className="text-6xl bg-green-300 h-screen">Reviews</div>;
+  return (
+    <div className="pt-20 pb-20 flex items-center justify-center flex-col bg-[#13357b]">
+      <div className="w-[80%] mx-auto grid grid-cols-1 lg:grid-cols-2 gap-10">
+        {/* Text Content */}
+        <div>
+          <h1 className="text-2xl font-semibold text-white">
+            What our customers are saying us?
+          </h1>
+          <p className="mt-6 text-gray-200">
+            We take pride in delivering exceptional travel experiences that
+            exceed our customers' expectations. Our commitment to quality
+            service and attention to detail has earned us the trust and loyalty
+            of travelers worldwide. Read what our valued customers have to say
+            about their journeys with us.
+          </p>
+          {/* Ratings */}
+          <div className="mt-6 flex items-center space-x-6">
+            <div>
+              <p className="text-2xl font-bold text-white">4.88</p>
+              <p className="text-white mb-2">Overall Rating</p>
+              <div className="flex items-center">
+                <FaStar className="text-amber-300" />
+                <FaStar className="text-amber-300" />
+                <FaStar className="text-amber-300" />
+                <FaStar className="text-amber-300" />
+                <FaStar className="text-white" />
+              </div>
+            </div>
+          </div>
+        </div>
+        {/* slider */}
+        <div className="overflow-hidden">
+          <ReviewSlider />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Reviews;
